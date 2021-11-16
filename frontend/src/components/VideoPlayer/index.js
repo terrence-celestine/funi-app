e37fefa972e3df8615f28148a5957527fef90e29
Terrence Celestine <terrencebutler89@gmail.com>
2021-11-15 19:54:10 -0800

    Updated Video Player
    Added Skip Intro button
    Added skip to previous and next episodes.
    Added cookie check to load last watched time for series.

--- frontend/src/components/VideoPlayer/index.js
+++ frontend/src/components/VideoPlayer/index.js
@@ -1,29 +1,75 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect, useRef } from "react";
+import { useNavigate } from "react-router-dom";
 import "./index.css";
 
 const VideoPlayer = () => {
+  const navigate = useNavigate();
   const { showHandle, episodeID } = useParams();
   const [isLoading, setIsLoading] = useState(true);
+  const [showData, setShowData] = useState([]);
   const [volumeBarStatus, setShowVolumeBar] = useState(false);
   const [overlayVisible, setOverlay] = useState(true);
   const [isPlaying, setPlayingStatus] = useState(false);
   const [isMuted, setPlayerVolume] = useState(false);
   const [playerTime, setPlayerTime] = useState("0:00");
-  const [totalTime, setTotalTime] = useState(0);
+  const [totalTime, setTotalTime] = useState("00:00");
+  const [showSettingsFlag, setSettingsVisible] = useState(false);
   const videoRef = useRef();
   const volumeRef = useRef();
+  const audioRef = useRef();
+  const progressBarRef = useRef();
+
+  useEffect(() => {
+    const episode_progress = localStorage.getItem("episode_progress");
+    const initialValue = JSON.parse(episode_progress);
+    var jumpToLastProgress = 0;
+    if (initialValue["episode"] === episodeID) {
+      jumpToLastProgress = initialValue["progress"];
+      videoRef.current.currentTime = jumpToLastProgress;
+    }
+    fetch(`/episodes/${showHandle}/${episodeID}/meta`)
+      .then((response) => response.json())
+      .then((data) => {
+        setShowData(data);
+        setIsLoading(false);
+      });
+  }, []);
+
+  const updateAudioTrack = () => {
+    pauseVideo();
+    const time = videoRef.current.currentTime;
+    audioRef.current.currentTime = time;
+    videoRef.current.muted = true;
+    playVideo();
+    audioRef.current.play();
+  };
   const playVideo = () => {
     videoRef.current.play();
   };
   const pauseVideo = () => {
+    console.log("setting cookie");
     videoRef.current.pause();
+    localStorage.setItem(
+      "episode_progress",
+      JSON.stringify({
+        handle: showHandle,
+        episode: episodeID,
+        progress: videoRef.current.currentTime,
+      })
+    );
+    console.log(
+      JSON.stringify({
+        handle: showHandle,
+        episode: episodeID,
+        progress: videoRef.current.currentTime,
+      })
+    );
   };
   const showOverlay = () => {
     setOverlay(true);
   };
   const hideOverlay = () => {
-    console.log(isPlaying);
     if (isPlaying === false) {
       setOverlay(true);
     } else {
@@ -46,7 +92,28 @@ const VideoPlayer = () => {
     var totalTimeStr = String(minutes + ":" + seconds);
     setPlayerTime(totalTimeStr);
     updateProgressBar(time);
+
+    if (Math.floor(time) === showData.intro_time) {
+      showSkipButton();
+    }
+
+    if (Math.floor(time) % 15 === 0) {
+      console.log("updating local cookie");
+      localStorage.setItem(
+        "episode_progress",
+        JSON.stringify({
+          handle: showHandle,
+          episode: episodeID,
+          progress: videoRef.current.currentTime,
+        })
+      );
+    }
   };
+
+  const showSkipButton = () => {
+    document.getElementById("skip-intro").classList.add("show-btn");
+  };
+
   const mutePlayer = () => {
     if (isMuted) {
       videoRef.current.muted = false;
@@ -62,9 +129,7 @@ const VideoPlayer = () => {
     document.getElementById("progressBar").value = timeVal;
   };
   const updatePlayerStatus = () => {
-    console.log("before", isPlaying);
     setPlayingStatus(!isPlaying);
-    console.log("after", isPlaying);
     if (!isPlaying) {
       playVideo();
     } else {
@@ -72,14 +137,14 @@ const VideoPlayer = () => {
     }
   };
   const rewindPlayer = () => {
-    var newTime = videoRef.current.currentTime - 30;
+    var newTime = videoRef.current.currentTime - 10;
     if (newTime < 0) {
       newTime = 0;
     }
     videoRef.current.currentTime = newTime;
   };
   const forwardPlayer = () => {
-    var newTime = videoRef.current.currentTime + 30;
+    var newTime = videoRef.current.currentTime + 10;
     if (newTime > videoRef.current.duration) {
       newTime = videoRef.current.duration;
     }
@@ -100,52 +165,112 @@ const VideoPlayer = () => {
     videoRef.current.volume = volumeValue;
   };
 
-  const goToNextEpisode = () => {};
+  const handleProgressBarClick = (e) => {
+    const value = e.clientX - videoRef.current.offsetLeft;
+    const seekto = (value / videoRef.current.offsetWidth) * 100;
+    progressBarRef.current.value = seekto;
+    videoRef.current.currentTime = (seekto / 100) * videoRef.current.duration;
+  };
+
+  const goToPrevEpisode = () => {
+    const episodeID = showData.id - 1;
+    navigate(`/v/${showHandle}/${episodeID}`, { replace: false });
+    window.location.reload();
+  };
+
+  const goToNextEpisode = () => {
+    const episodeID = showData.id + 1;
+    navigate(`/v/${showHandle}/${episodeID}`, { replace: false });
+    window.location.reload();
+  };
+
+  const showSettings = () => {
+    setSettingsVisible(!showSettingsFlag);
+  };
+
+  const returnToShowDetails = () => {
+    navigate(`/shows/${showHandle}`, { replace: true });
+  };
+
+  const skipIntro = () => {
+    videoRef.current.currentTime = showData.jump_time;
+    document.querySelector("#skip-intro").classList.remove("show-btn");
+  };
 
   const playIcon = require(`../../assets/video-player/play.png`);
   const pauseIcon = require(`../../assets/video-player/pause.png`);
   const volumeIcon = require(`../../assets/video-player/volume.png`);
   const mutedIcon = require(`../../assets/video-player/muted.png`);
   const languagesIcon = require(`../../assets/video-player/languages.png`);
-  const rewindIcon = require(`../../assets/video-player/rewind.png`);
-  const backIcon = require(`../../assets/video-player/back.png`);
-  const fwrdIcon = require(`../../assets/video-player/forward.png`);
+  const rewindIcon = require(`../../assets/video-player/back.png`);
+  const backIcon = require(`../../assets/video-player/close_player.png`);
+  const fwrdIcon = require(`../../assets/video-player/fwrd.png`);
   const fullscreenIcon = require(`../../assets/video-player/fullscreen.png`);
   const settingsIcon = require(`../../assets/video-player/settings.png`);
-
+  const previousIcon = require(`../../assets/video-player/prev_episode.png`);
+  const nextIcon = require(`../../assets/video-player/next_episode.png`);
+  const pauseCenterd = require(`../../assets/video-player/pause_centered.png`);
+  const playCenterd = require(`../../assets/video-player/play_centered.png`);
   return (
     <>
-      {isLoading === false ? (
-        ""
-      ) : (
-        <div
-          className="videoWrapper"
-          onMouseEnter={showOverlay}
-          onMouseLeave={hideOverlay}
-        >
-          <div id="infoOverlay" className={overlayVisible ? "showOverlay" : ""}>
-            <div className="top">
-              <div className="top-container">
-                <div
-                  className="go-back"
-                  onClick={() => alert("clicked go back")}
-                >
-                  <img src={backIcon.default} alt="go back" />
-                </div>
-                <div className="meta">
-                  <h2>{showHandle}</h2>
-                  <span>Season 1 Episode {episodeID} - Episode Title</span>
-                  <span>Language</span>
-                  <span>Rating</span>
-                </div>
+      <div
+        className="videoWrapper"
+        onMouseEnter={showOverlay}
+        onMouseLeave={hideOverlay}
+      >
+        <div id="infoOverlay" className={overlayVisible ? "showOverlay" : ""}>
+          <div className="top">
+            <div className="top-container">
+              <div className="go-back" onClick={() => returnToShowDetails()}>
+                <img src={backIcon.default} alt="go back" />
+              </div>
+              <div className="meta">
+                <h2>{showHandle.replace("-", " ").toUpperCase()}</h2>
+                <span className="title">
+                  Season 1 Episode {episodeID} - {showData.title}
+                </span>
+                {/* <span>
+                    {showData.languages.map((lang) => (
+                      <span>{lang}</span>
+                    ))}
+                  </span> */}
+                <span className="rating">{showData.rating}</span>
               </div>
             </div>
-            <div className="bottom">
+          </div>
+          <div className="playCentered" onClick={() => updatePlayerStatus()}>
+            {isPlaying ? (
+              <img src={pauseCenterd.default} alt="" />
+            ) : (
+              <img src={playCenterd.default} alt="" />
+            )}
+          </div>
+          <div className="bottom">
+            <div id="settingsBox" class={showSettingsFlag ? "show" : ""}>
+              <span>this is the settings box</span>
+            </div>
+            <button id="skip-intro" onClick={() => skipIntro()}>
+              Skip Intro
+            </button>
+            <div className="controls-container">
               <div className="progress-bar">
-                <progress id="progressBar" value="0" max="100" />
+                <progress
+                  ref={progressBarRef}
+                  onClick={(e) => handleProgressBarClick(e)}
+                  id="progressBar"
+                  value="0"
+                  max="100"
+                />
               </div>
               <div className="controls">
                 <div className="left">
+                  {showData.prev_episode ? (
+                    <div className="prev" onClick={() => goToPrevEpisode()}>
+                      <img src={previousIcon.default} alt="" />
+                    </div>
+                  ) : (
+                    ""
+                  )}
                   <div
                     className="volume"
                     onMouseEnter={showVolumeBar}
@@ -198,40 +323,50 @@ const VideoPlayer = () => {
                   <div className="full-screen">
                     <img src={fullscreenIcon.default} alt="" />
                   </div>
-                  <div className="languages">
+                  <div className="languages" onClick={() => updateAudioTrack()}>
                     <img src={languagesIcon.default} alt="" />
                   </div>
-                  <div className="settings">
+                  <div className="settings" onClick={() => showSettings()}>
                     <img src={settingsIcon.default} alt="" />
                   </div>
-                  <div className="next" onClick={() => goToNextEpisode()}>
-                    <img src={fwrdIcon.default} alt="" />
-                  </div>
+                  {showData.next_episode ? (
+                    <div className="next" onClick={() => goToNextEpisode()}>
+                      <img src={nextIcon.default} alt="" />
+                    </div>
+                  ) : (
+                    ""
+                  )}
                 </div>
               </div>
             </div>
           </div>
-          <video
-            ref={videoRef}
-            preload="metadata"
-            onLoadedMetadata={(e) => updatePlayerTime(e.target.duration)}
-            onTimeUpdate={(e) => parseTime(e.target.currentTime)}
-            onLoadedData={() => updatePlayerStatus()}
-            controls={false}
-          >
-            <source
-              type="video/mp4"
-              src={`/episodes/${showHandle}/${episodeID}`}
-            />
-            <track
-              label="English"
-              kind="subtitles"
-              srcLang="jp"
-              src={`/subtitles/demon-slayer/ep1`}
-            ></track>
-          </video>
         </div>
-      )}
+        <video
+          ref={videoRef}
+          preload="metadata"
+          onLoadedMetadata={(e) => updatePlayerTime(e.target.duration)}
+          onTimeUpdate={(e) => parseTime(e.target.currentTime)}
+          onLoadedData={() => updatePlayerStatus()}
+          controls={false}
+        >
+          <source
+            type="video/mp4"
+            src={`/episodes/${showHandle}/${episodeID}`}
+          />
+          <track
+            label="English"
+            kind="subtitles"
+            srcLang="jp"
+            src={`/subtitles/demon-slayer/ep1`}
+          ></track>
+        </video>
+        <audio ref={audioRef} id="a1" mediaGroup="a11y_vid">
+          <source
+            src={`/episodes/${showHandle}/${episodeID}/audio`}
+            type="audio/aac"
+          />
+        </audio>
+      </div>
     </>
   );
 };
--- server.js
+++ server.js
@@ -3,11 +3,322 @@ const app = express(); //Line 2
 const fs = require("fs");
 const port = process.env.PORT || 5000; //Line 3
 
+const all_shows = {
+  demon_slayer: {
+    episodes: [
+      {
+        id: 1,
+        title: "Cruelty",
+        intro_time: 57,
+        jump_time: 88,
+        duration: "23:39",
+        thumbnail: "ep1.jpg",
+        rating: "TV-14",
+        prev_episode: false,
+        next_episode: true,
+        languages: ["english", "japanese"],
+      },
+      {
+        id: 2,
+        thumbnail: "ep2.jpg",
+        title: "Trainer Sakonji Urodaki",
+        duration: "23:39",
+        intro_time: 57,
+        jump_time: 88,
+        rating: "TV-14",
+        prev_episode: true,
+        next_episode: true,
+        languages: ["english", "japanese"],
+      },
+      {
+        id: 3,
+        title: "Sabito and Makomo",
+        duration: "23:39",
+        thumbnail: "ep3.jpg",
+        rating: "TV-14",
+        prev_episode: true,
+        next_episode: true,
+        languages: ["english", "japanese"],
+      },
+      {
+        id: 4,
+        thumbnail: "ep4.jpg",
+        title: "Final Selection",
+        duration: "23:39",
+        rating: "TV-14",
+        prev_episode: true,
+        next_episode: true,
+        languages: ["english", "japanese"],
+      },
+      {
+        id: 5,
+        title: "My Own Steel",
+        duration: "23:39",
+        thumbnail: "ep5.jpg",
+        rating: "TV-14",
+        prev_episode: true,
+        next_episode: true,
+        languages: ["english", "japanese"],
+      },
+      {
+        id: 6,
+        thumbnail: "ep6.jpg",
+        title: "Swordsman Accompanying a Demon",
+        duration: "23:39",
+        rating: "TV-14",
+        prev_episode: true,
+        next_episode: true,
+        languages: ["english", "japanese"],
+      },
+      {
+        id: 7,
+        title: "Muzan Kibutsuji",
+        duration: "23:39",
+        thumbnail: "ep7.jpg",
+        rating: "TV-14",
+        prev_episode: true,
+        next_episode: true,
+        languages: ["english", "japanese"],
+      },
+      {
+        id: 8,
+        thumbnail: "ep8.jpg",
+        title: "The Smell of Enchanting Blood",
+        duration: "23:39",
+        rating: "TV-14",
+        prev_episode: true,
+        next_episode: true,
+        languages: ["english", "japanese"],
+      },
+      {
+        id: 9,
+        title: "Temari Demon and Arrow Demon",
+        duration: "23:39",
+        thumbnail: "ep9.jpg",
+        rating: "TV-14",
+        prev_episode: true,
+        next_episode: true,
+        languages: ["english", "japanese"],
+      },
+      {
+        id: 10,
+        thumbnail: "ep10.jpg",
+        title: "Together Forever",
+        duration: "23:39",
+        rating: "TV-14",
+        prev_episode: true,
+        next_episode: true,
+        languages: ["english", "japanese"],
+      },
+      {
+        id: 11,
+        title: "Tsuzumi Mansion",
+        duration: "23:39",
+        thumbnail: "ep11.jpg",
+        rating: "TV-14",
+        prev_episode: true,
+        next_episode: true,
+        languages: ["english", "japanese"],
+      },
+      {
+        id: 12,
+        thumbnail: "ep12.jpg",
+        title: "The Boar Bares Its Fangs, Zenitsu Sleeps",
+        duration: "23:39",
+        rating: "TV-14",
+        prev_episode: true,
+        next_episode: true,
+        languages: ["english", "japanese"],
+      },
+      {
+        id: 13,
+        title: "Something More Important Than Life",
+        duration: "23:39",
+        thumbnail: "ep13.jpg",
+        rating: "TV-14",
+        prev_episode: true,
+        next_episode: true,
+        languages: ["english", "japanese"],
+      },
+      {
+        id: 14,
+        thumbnail: "ep14.jpg",
+        title: "The House with the Wisteria Family Crest",
+        duration: "23:39",
+        rating: "TV-14",
+        prev_episode: true,
+        next_episode: true,
+        languages: ["english", "japanese"],
+      },
+      {
+        id: 15,
+        title: "Mount Natagumo",
+        duration: "23:39",
+        thumbnail: "ep15.jpg",
+        rating: "TV-14",
+        prev_episode: true,
+        next_episode: true,
+        languages: ["english", "japanese"],
+      },
+      {
+        id: 16,
+        thumbnail: "ep16.jpg",
+        title: "Letting Someone Else Go First",
+        duration: "23:39",
+        rating: "TV-14",
+        prev_episode: true,
+        next_episode: true,
+        languages: ["english", "japanese"],
+      },
+      {
+        id: 17,
+        thumbnail: "ep17.jpg",
+        title: "Letting Someone Else Go First",
+        duration: "23:39",
+        rating: "TV-14",
+        prev_episode: true,
+        next_episode: true,
+        languages: ["english", "japanese"],
+      },
+      {
+        id: 18,
+        thumbnail: "ep18.jpg",
+        title: "Letting Someone Else Go First",
+        duration: "23:39",
+        rating: "TV-14",
+        prev_episode: true,
+        next_episode: true,
+        languages: ["english", "japanese"],
+      },
+      {
+        id: 19,
+        thumbnail: "ep19.jpg",
+        title: "Letting Someone Else Go First",
+        duration: "23:39",
+        rating: "TV-14",
+        prev_episode: true,
+        next_episode: true,
+        languages: ["english", "japanese"],
+      },
+      {
+        id: 20,
+        thumbnail: "ep20.jpg",
+        title: "Letting Someone Else Go First",
+        duration: "23:39",
+        rating: "TV-14",
+        prev_episode: true,
+        next_episode: true,
+        languages: ["english", "japanese"],
+      },
+      {
+        id: 21,
+        thumbnail: "ep21.jpg",
+        title: "Letting Someone Else Go First",
+        duration: "23:39",
+        rating: "TV-14",
+        prev_episode: true,
+        next_episode: true,
+        languages: ["english", "japanese"],
+      },
+      {
+        id: 22,
+        thumbnail: "ep22.jpg",
+        title: "Letting Someone Else Go First",
+        duration: "23:39",
+        rating: "TV-14",
+        prev_episode: true,
+        next_episode: true,
+        languages: ["english", "japanese"],
+      },
+      {
+        id: 23,
+        thumbnail: "ep23.jpg",
+        title: "Letting Someone Else Go First",
+        duration: "23:39",
+        rating: "TV-14",
+        prev_episode: true,
+        next_episode: true,
+        languages: ["english", "japanese"],
+      },
+      {
+        id: 24,
+        thumbnail: "ep24.jpg",
+        title: "Letting Someone Else Go First",
+        duration: "23:39",
+        rating: "TV-14",
+        prev_episode: true,
+        next_episode: true,
+        languages: ["english", "japanese"],
+      },
+      {
+        id: 25,
+        thumbnail: "ep25.jpg",
+        title: "Letting Someone Else Go First",
+        duration: "23:39",
+        rating: "TV-14",
+        prev_episode: true,
+        next_episode: true,
+        languages: ["english", "japanese"],
+      },
+      {
+        id: 26,
+        thumbnail: "ep26.jpg",
+        title: "Letting Someone Else Go First",
+        duration: "23:39",
+        rating: "TV-14",
+        prev_episode: true,
+        languages: ["english", "japanese"],
+      },
+    ],
+  },
+};
+
 app.get("/subtitles/:showHandle/:episodeID", (req, res) => {
   res.sendFile(
     `./videos/episodes/${req.params.showHandle}/subtitles/${req.params.episodeID}.vtt`,
     { root: __dirname }
   );
+});
+
+// get audio track
+app.get("/episodes/:showHandle/:episodeID/audio", (req, res) => {
+  const range = req.headers.range;
+  const videoPath = `./audio/episodes/${req.params.showHandle}/${req.params.episodeID}/jp.aac`;
+  const videoSize = fs.statSync(videoPath).size;
+
+  const chunkSize = 1 * 1e6;
+  var start = 0;
+  if (range) {
+    start = Number(range.replace(/\D/g, ""));
+  } else {
+    start = Number(0);
+  }
+
+  const end = Math.min(start + chunkSize, videoSize - 1);
+
+  const contentLength = end - start + 1;
+
+  const headers = {
+    "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+    "Accept-Ranges": "bytes",
+    "Content-Length": contentLength,
+    "Content-Type": "video/mp4",
+  };
+
+  res.writeHead(206, headers);
+
+  const stream = fs.createReadStream(videoPath, { start, end });
+
+  return stream.pipe(res);
+});
+
+// meta data endpoint
+app.get("/episodes/:showHandle/:episodeID/meta", (req, res) => {
+  const handle = `${req.params.showHandle}`.replace("-", "_");
+  const foundEpisode = all_shows[handle]["episodes"].find(
+    (episode) => episode.id == req.params.episodeID
+  );
+  return res.send(foundEpisode);
 });
 
 app.get("/episodes/:showHandle/:episodeID", (req, res) => {
